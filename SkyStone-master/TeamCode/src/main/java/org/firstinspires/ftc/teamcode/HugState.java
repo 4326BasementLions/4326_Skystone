bdc2390df4363eaaa700c8191c16260c29d0fdc6
hmftc <HMFTC@rob-03.ntd.horacemann.org>
2019-09-29 13:45:03 -0400

    hugState is now enncoder based

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HugState.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HugState.java
@@ -26,6 +26,13 @@ import java.util.Locale;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class HugState implements State{
+    int newleftBackTarget;
+    int newrightBackTarget;
+    int  newleftFrontTarget;
+    int  newrightFrontTarget;
+    int pulleyTarget;
+    double distance;
+
     State NextState;
 
     DcMotor leftFront;                                    //////////////////////
@@ -43,16 +50,69 @@ public class HugState implements State{
 
     int time;
 
+    double pow;
 
+    boolean open;
+
+
+    static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: Andymark Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static double driveSpeed = 0.6;
+    static final double TURN_SPEED = 0.5;
 
 
 
     //does the action at start!
+    public HugState(ArrayList<DcMotor> motor, int speed /*let's make this encoder based asap*/, Servo left, Servo right, double Pow){//, boolean tf){
+        driveSpeed = speed;
+        leftFront = motor.get(0);
+        rightFront = motor.get(1);
+        leftBack = motor.get(2);
+        rightBack = motor.get(3);
+        pulley = motor.get(4);
+
+        leftHand = left;
+        rightHand = right;
+
+//        time = sec;
+
+        //open = tf;
+
+        pow = Pow;
+
+    }
 
 
 
     @Override
     public void start() {
+        //Reset the encoders back to zero for the next movement
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        pulley.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+//        rightBack.setDirection(DcMotor.Direction.REVERSE);
+//        rightFront.setDirection(DcMotor.Direction.REVERSE);
+
+        //Bring them back to using encoders
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pulley.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //Setting their target to their current encoder value (should be zero) to the amount of inches times the counts per inches
+
+        newleftBackTarget = leftBack.getCurrentPosition() + (int) (distance * COUNTS_PER_INCH);
+        newrightBackTarget = rightBack.getCurrentPosition() + (int) (distance * COUNTS_PER_INCH);
+        newleftFrontTarget = leftFront.getCurrentPosition() + (int) (distance * COUNTS_PER_INCH);
+        newrightFrontTarget = rightFront.getCurrentPosition() + (int) (distance * COUNTS_PER_INCH);
+        pulleyTarget = pulley.getCurrentPosition() + (int) (distance * COUNTS_PER_INCH);
 
         mRuntime.reset();
 
@@ -60,15 +120,36 @@ public class HugState implements State{
 
     @Override
     public State update() {
-        //leftFront.setPower();
 
-        if(mRuntime.seconds()<=3) {
+        if(pulleyTarget > pulley.getCurrentPosition()) {
+            pulley.setPower(driveSpeed);
             leftHand.setPosition(-1);
             rightHand.setPosition(1);
+            return this;
         }
-        if(mRuntime.seconds()<5&&mRuntime.seconds()>3) {
-            pulley.setPower(1);
-        }
+
+        //if(mRuntime.seconds()<time){
+//            if(open){
+//                leftHand.setPosition(1);
+//                rightHand.setPosition(-1);
+//            }
+//            else{
+//                leftHand.setPosition(-1);
+//                rightHand.setPosition(1);
+//            }
+//            pulley.setPower(pow);
+
+            //return this; //>_<
+       // }
+        //leftFront.setPower();
+
+//        if(mRuntime.seconds()<=3) {
+//            leftHand.setPosition(-1);
+//            rightHand.setPosition(1);
+//        }
+//        if(mRuntime.seconds()<5&&mRuntime.seconds()>3) {
+//            pulley.setPower(1);
+//        }
 
 
 
