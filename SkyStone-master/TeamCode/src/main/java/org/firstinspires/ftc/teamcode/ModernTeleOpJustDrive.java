9bd478160b7e13b39a0c3b3b7906265358b352a5
Eleanor Mather <eleanor_mather@horacemann.org>
2020-01-27 11:36:26 -0500

    Add files via upload

---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ModernTeleOpJustDrive.java
@@ -0,0 +1,150 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@TeleOp(name="Modern Tele op JUST DRIVE", group="Iterative Opmode")
+public class ModernTeleOpJustDrive extends OpMode {
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+
+    Servo foundationLeft;
+    Servo blockServo;
+
+    DcMotor pulley;
+    Servo twist;
+    Servo grab;
+
+    DcMotor intakeLeft;
+    DcMotor intakeRight;
+
+    double x;
+    double y;
+
+    boolean grip = false;
+
+
+    @Override
+    public void init() {
+        leftBack = hardwareMap.dcMotor.get("left_back");
+        rightBack = hardwareMap.dcMotor.get("right_back");
+        leftFront = hardwareMap.dcMotor.get("left_front");
+        rightFront = hardwareMap.dcMotor.get("right_front");
+
+        foundationLeft = hardwareMap.servo.get("foundation");
+        //foundationRight = hardwareMap.servo.get("foundation_right");
+
+        blockServo = hardwareMap.servo.get("block");
+
+        pulley = hardwareMap.dcMotor.get("pulley");
+       // twist = hardwareMap.servo.get("twist");  //not configged yet
+      //  grab = hardwareMap.servo.get("grab"); //not configged yet
+
+        intakeLeft = hardwareMap.dcMotor.get("intake_left");
+//        intakeRight = hardwareMap.dcMotor.get("intake_right");
+
+
+        x=0;
+
+        y=1;
+
+    }
+
+    @Override
+    public void start() {
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+//        foundationRight.setPosition(x);
+//        foundationLeft.setPosition(x);
+//        twist.setPosition(y);
+    }
+
+    @Override
+    public void loop() {
+
+        //GAMEPAD 1
+        //Drive train
+        float drive = gamepad1.right_stick_y;
+        float strafe = gamepad1.right_stick_x;
+        float turn = gamepad1.left_stick_x;
+
+        float fl = drive - strafe + turn; //drive - strafe + turn;
+        float fr = drive - strafe - turn;
+        float bl = drive + strafe + turn; // drive + strafe + turn;
+        float br = drive + strafe - turn;
+
+        leftFront.setPower(fl);
+        rightFront.setPower(fr);
+        leftBack.setPower(bl);
+        rightBack.setPower(br);
+////
+//////        //Foundation Clasps (triggers)
+//////
+
+//        // foundationRight.setPosition(x);
+////
+        if(gamepad1.left_trigger>0){
+        //    x+=1;
+            foundationLeft.setPosition(1);
+        }
+        if(gamepad1.right_trigger>0){
+          //  x+=-1;
+            foundationLeft.setPosition(-1);
+        }
+       // foundationLeft.setPosition(x); //   :(
+
+        //put blockServo code here
+        if (gamepad2.right_trigger>0){
+            blockServo.setPosition(.25);
+        }
+        if (gamepad2.left_trigger>0){
+            blockServo.setPosition(.8);
+        }
+//
+//////
+//////    //GAMEPAD 2
+//////        //Intake
+//        if(gamepad2.left_trigger>0){
+//            intakeRight.setPower(1);
+//            intakeLeft.setPower(1);
+//        }
+//        if(gamepad2.right_trigger>0){
+//            intakeRight.setPower(-1);
+//            intakeLeft.setPower(-1);
+//        }
+//////
+//////        //Pulley
+        pulley.setPower(gamepad2.right_stick_y/4);
+//////
+//////        //Twist+Grab
+//        if(gamepad2.left_stick_x>0&&!(y>=1)){ //flick right
+//            y=1;
+//        }
+//        if(gamepad2.left_stick_x<0&&!(y<=-1)){  //flick left
+//            y=-1;
+//        }
+//////
+//        twist.setPosition(y);
+//
+//        if(gamepad2.right_bumper&&grip){
+//            grab.setPosition(0);  //let go of block
+//            grip = false;
+//        }
+//        if(gamepad2.right_bumper&&!grip){
+//            grab.setPosition(1);  //hold on to block
+//            grip = true;
+//        }
+//        telemetry.addData("number", gamepad1.right_trigger);
+
+
+    }
+}
